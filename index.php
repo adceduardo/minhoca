e9cd3b1621d4a6e13248c80beaf75ef956e1f275
EDUARDO ALVES DE CARVALHO <105119276+adceduardo@users.noreply.github.com>
2024-10-13 01:56:26 -0300

    Update and rename minhoca.php to index.php

--- index.php
+++ index.php
@@ -5,14 +5,15 @@
         $caracteres = mb_str_split($string);                                //Divide a string em caracteres e armazena em um array
         $resultados = [];                                                   //Cria um array para armazenar as novas strings      
                                                                                 
-        for ($i = 0; $i < $n; $i++) {                   
+        for ($i = 0; $i < $n; $i++)
+        {                   
             $caracteres[$i] = mb_strtoupper($caracteres[$i]);               //Transforma o caractere atual em maiúsculo
-                                                                            
+                                                                          
             $resultados[] = implode('', $caracteres);                       //Une os caracteres em uma nova string e adiciona ao array de resultados
             $caracteres[$i] = mb_strtolower($caracteres[$i]);               //Retorna o caractere atual para minusculo
         }
                                                                 
-        foreach($resultados as $resultado)                                 //Imprime todas as strings resultantes
+        foreach($resultados as $resultado)                                  //Imprime todas as strings resultantes
         {                               
             echo $resultado . "<br>";
         }
